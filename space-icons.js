4dedec1d6b9b7e650975c64c1c4332ab86d5e2b5
Edward Medvedev <edward.medvedev@gmail.com>
2018-02-14 18:48:04 +0100

    Cubesat icon!

---
+++ space-icons.js
@@ -0,0 +1,13 @@
+// Icons courtesy of freepik.com
+
+import generic from './icons/generic.js';
+import sputnik from './icons/sputnik.js';
+import cubesat from './icons/cubesat.js';
+import cute from './icons/cute.js';
+
+export default {
+  generic,
+  cubesat,
+  sputnik,
+  cute,
+};
